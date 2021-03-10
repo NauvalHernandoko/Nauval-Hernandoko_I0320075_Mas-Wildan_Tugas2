9e7e8467b5b3602c223e693b1b908ba367c91f02
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-03-10 21:52:50 +0700

    Soal 2 (tugas)

---
+++ Soal 2 (tugas).py
@@ -0,0 +1,71 @@
+#tipe data string
+tipe_data = "tipe data string"
+print("bertipe:" ,tipe_data)
+nama_lengkap = "Nauval Hernandoko"
+print("nama lengkap:" ,nama_lengkap)
+jenis_kelamin = "Laki-laki"
+print("jenis kelamin:" ,jenis_kelamin)
+alamat = """
+    Tunjungsari, Mlese, Gantiwarno, Klaten
+    """""
+print("alamat:" ,alamat)
+agama = "Islam"
+print("agama: " ,agama)
+riwayat_pendidikan = """
+        SD N Gesikan, SMP N 1 Wedi, SMA N 1 Klaten, Teknik Industri UNS
+        """""
+print("riwayat pendidikan:" ,riwayat_pendidikan)
+hobi = "Bermain musik dan futsal"
+print("hobi:" ,hobi)
+nama_orang_tua = "Sadhaka Budiyanto"
+print("nama orang tua:" ,nama_orang_tua)
+pekerjaan_orang_tua = "Guru"
+print("pekerjaan orang tua:" ,pekerjaan_orang_tua)
+print(" ")
+
+#tipe data integer
+tipe_data2 = "tipe data integer"
+print("bertipe:" ,tipe_data2)
+
+umur_sekarang = 18
+print("umur:" ,umur_sekarang)
+umur_bulan = umur_sekarang * 12 + 11                               #menghitung umur dalam bulan
+print("umur dalam bulan:" ,umur_bulan)
+umur_hari1 = umur_bulan * 30                                       #asumsi 1 bulan 30 hari
+print("umur dalam hari1:" ,umur_hari1)
+umur_hari2 = umur_bulan * 31                                       #asumsi 1 bulan 31 hari
+print("umur dalam hari2:" ,umur_hari2)
+print("umur dalam hari1 < umur dalam hari < umur dalam hari2")
+umur_hari = 18 * 365 + (365-31)                                    #asumsi 1 tahun 365 hari
+print("umur hari (perhitungan tahun):" ,umur_hari)
+
+tinggi_badan = 174
+print("tinggi badan:" ,tinggi_badan)
+ukuran_sepatu = 41
+print("ukuran sepatu:" ,ukuran_sepatu)
+berat_badan = 56
+print("berat badan:" ,berat_badan)
+print(" ")
+
+#tipw data float
+tipe_data3 = "tipe data float"
+print("bertipe:" ,tipe_data3)
+
+tinggi_badan = 174                                                                          #variabel TB
+berat_badan_ideal = (float(tinggi_badan) - 100) - ((float(tinggi_badan) - 100) * 10 / 100)  #menghitung BBI
+print("BBI:" ,berat_badan_ideal)
+
+jarak_meter = 40000                                 #variabel s (jarak)
+waktu_sekon = 6600                                  #variabel t (waktu)
+kecepatan = float(jarak_meter) / float(waktu_sekon) #rumus v (kecepatan)
+print("kecepatan dari rumah ke UNS: " ,kecepatan)
+
+
+
+
+
+
+
+
+
+
