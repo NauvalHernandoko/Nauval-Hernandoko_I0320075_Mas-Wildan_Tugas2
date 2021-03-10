849818ae8e3f19537a19b6626c31fdfa0d2fce97
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-03-10 10:40:36 +0700

    Tugas Week 2 Praktikum Programa Komputer

--- Soal 1 (tugas).py
+++ Soal 1 (tugas).py
@@ -13,19 +13,19 @@ L_lingkaran = 3.14 * r * r
 print("\nLuas lingkaran =\n" ,L_lingkaran)
 
 #menghitung luas kubus
-print("\nMenghitung luas permukaan kubus")
+print("\nMenghitung luas permukaan kubus\n")
 s = float(input("masukkan panjang sisi: "))
 L_kubus = 6 * s * s
 print("\nLuas permukaan kubus =\n" ,L_kubus)
 
 #mengkonversi suhu celcius ke fahrenheit
-print("\nMengkonversi suhu celcius ke fahrenheit")
+print("\nMengkonversi suhu celcius ke fahrenheit\n")
 c = float(input("masukkan nilai celcius: "))
 F = (c * 9/5) + 32
 print("\nC =\n" ,F)
 
 #mengkonversi suhu reamur ke kelvin
-print("\nMengkonversi suhu reamur ke kelvin")
+print("\nMengkonversi suhu reamur ke kelvin\n")
 R = float(input("masukkan nilai reamur: "))
 K = (5 / 4 * R) + 273
 print("K =" ,K)
